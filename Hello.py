068fd9c5cee65a5b165be74fdba40905ec96dad7
tranmanhcuong253 <cuonghb243@gmail.com>
2024-11-14 17:29:43 +0700

    Initial commit

---
+++ Hello.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Vietnamese RAG Chatbot")
+
+    
+st.write("### AI for devs 02 Project - Trần Mạnh Cường")
+
+st.markdown(
+    """
+        List Backend was listed here:
+        - Chabot Backend: [Link Colab](https://colab.research.google.com/drive/1eVIFJfTzQZBz-bkuvX3KgnjK07hngs8M?usp=sharing)
+
+    """
+)
